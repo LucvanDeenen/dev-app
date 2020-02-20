8c76386285075edc10d15cd29a7ec1e538c23e38
Luc <Lucvd@hotmail.nl>
2020-02-20 15:34:46 +0100

    header added to menu

--- components/Menu.js
+++ components/Menu.js
@@ -1,28 +1,33 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native';
+import { Header } from 'react-native-elements';
 
 // const widthScreen = Dimensions.get("window").width;
 
 const Menu = props => {
     return (
         <View style={styles.menuTab}>
-            <TouchableOpacity>
-                <Text h1>Wo3w</Text>
+            <Header
+                leftComponent={{ icon: 'menu', color: '#fff' }}
+                centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
+                rightComponent={{ icon: 'home', color: '#fff' }}
+            />
+            <TouchableOpacity style={styles.opacityButton}>
+                <Text>Wo3w</Text>
             </TouchableOpacity>
         </View>
     );
 }
 
-const styles = StyleSheet.create ({
+const styles = StyleSheet.create({
     opacityButton: {
         borderStyle: "solid",
-        borderWidth: 1
+        borderWidth: 1,
+        padding: 5
     },
     menuTab: {
         borderStyle: "solid",
         borderWidth: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
         flex: 1,
     }
 });
