77b2fd37032d6781eb2d388070c54d118389a705
Luc <Lucvd@hotmail.nl>
2020-02-21 16:19:36 +0100

    added content

---
+++ components/Notepad/Content.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+export default class Content extends Component { 
+
+}
+
+const styles = StyleSheet.create({
+
+})
