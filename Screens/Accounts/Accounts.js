9829113bf2663e173c392a64ea7254e4851429ea
Luc <Lucvd@hotmail.nl>
2020-03-09 17:31:25 +0100

    working on db and setting up acc

--- Screens/Accounts/Accounts.js
+++ Screens/Accounts/Accounts.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, TouchableOpacity, Text } from 'react-native';
 
 import Firebase from '../../database/firebase.js';
+
+import AccountDetail from '../../components/Accounts/AccountDetails';
 
 // Styling
 import styles from './local-styles.js';
@@ -11,6 +13,7 @@ export default function Accounts(props) {
     const [accounts, setAccounts] = useState([]);
 
     const getAccounts = async () => {
+        setAccounts([]);
         const snapshot = await Firebase.getAccounts();
         snapshot.forEach(item => {
             console.log(item.data());
@@ -24,32 +27,42 @@ export default function Accounts(props) {
     }
 
     const pushAccounts = () => {
-        let json = {
-            Name: 'NewName',
-            Password: '123',
-            id: 2,
-        }
-        console.log(json);
         Firebase.addAccount(json);
     }
+
+    // useEffect(() => {
+    //     getAccounts();
+    // })
 
     if (props.control == 'accounts') {
         return (
             <View style={{ flex: 1 }}>
-                <TouchableOpacity style={{ backgroundColor: 'red', flex: 1, alignItems: 'center', justifyContent: 'center' }}
-                    onPress={() => { getAccounts() }}>
-                    <Text>Pull</Text>
-                </TouchableOpacity>
+                <View style={{ flex: 1, borderBottomWidth: 2, flexDirection: 'row' }}>
+                    <TouchableOpacity style={{ backgroundColor: 'red', flex: 1, alignItems: 'center', justifyContent: 'center' }}
+                        onPress={() => { getAccounts() }}>
+                        <Text>Pull</Text>
+                    </TouchableOpacity>
 
-                <TouchableOpacity style={{ backgroundColor: 'blue', flex: 1, alignItems: 'center', justifyContent: 'center' }}
-                    onPress={() => { pushAccounts() }}>
-                    <Text>Push</Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity style={{ backgroundColor: 'white', flex: 1, alignItems: 'center', justifyContent: 'center' }}
-                    onPress={() => { console.log(accounts) }}>
-                    <Text>List</Text>
-                </TouchableOpacity>
+                    <TouchableOpacity style={{ backgroundColor: 'blue', flex: 1, alignItems: 'center', justifyContent: 'center' }}
+                        onPress={() => { pushAccounts() }}>
+                        <Text>Push</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={{ flex: 5 }}>
+                    {
+                        accounts.map(account => {
+                            return <View style={styles.accountDetails} key={account.id}>
+                                <Text> {account.Name} </Text>
+                                <Text> {account.Password} </Text>
+                            </View>
+                        })
+                    }
+                    {/* <FlatList
+                        data={accounts}
+                        renderItem={({ account }) => (
+                            <NoteItems item={item} removeItem={removeItem} openItem={openItem} />
+                        )} /> */}
+                </View>
             </View>
         );
     } else {
---
+++ components/Accounts/AccountDetails.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+
+export default function AccountDetails(props) {
+    const [items, setItems] = useState([]);
+
+    return (
+        <View>
+            {
+            }
+        </View>
+    )
+}
